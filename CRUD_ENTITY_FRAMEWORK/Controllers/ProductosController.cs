e67bd1a2c85358b64c234b345daba4d97cdbaab9
Roberth Cobeña <tonnyco93@gmail.com>
2021-01-14 23:52:24 -0500

    actualizacion

--- CRUD_ENTITY_FRAMEWORK/Controllers/ProductosController.cs
+++ CRUD_ENTITY_FRAMEWORK/Controllers/ProductosController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CRUD_ENTITY_FRAMEWORK.Models;
+using CRUD_ENTITY_FRAMEWORK.Controllers;
+using CRUD_ENTITY_FRAMEWORK.Models.ViewModels;
 
 namespace CRUD_ENTITY_FRAMEWORK.Controllers
 {
@@ -11,7 +14,21 @@ namespace CRUD_ENTITY_FRAMEWORK.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            return View();
+            List<ListProductosViewModel> lst;
+            using (PracticaEntities db = new PracticaEntities())
+            {
+                lst = (from d in db.Productos
+                           select new ListProductosViewModel
+                           {
+                               Id = d.Id,
+                               Nombre = d.Nombre,
+                               Descripcion = d.Descripcion,
+                               Marca = d.Marca,
+                               Precio = (double)d.Precio,
+                               Stock = (int)d.Stock
+                           }).ToList();
+            }
+            return View(lst);
         }
     }
 }
--- CRUD_ENTITY_FRAMEWORK/Models/ViewModels/ListProductosViewModel.cs
+++ CRUD_ENTITY_FRAMEWORK/Models/ViewModels/ListProductosViewModel.cs
@@ -7,6 +7,11 @@ namespace CRUD_ENTITY_FRAMEWORK.Models.ViewModels
 {
     public class ListProductosViewModel
     {
-        public int Id{ get}
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
     }
 }
